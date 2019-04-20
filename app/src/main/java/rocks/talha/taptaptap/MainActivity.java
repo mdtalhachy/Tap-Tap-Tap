7c5d4687aecadf694442673e9f29c4d3de62000b
MD Talha Chowdhury <talhachowdhury3.1416@gmail.com>
2019-04-20 11:05:19 +0600

    Background Improved

--- app/src/main/java/rocks/talha/taptaptap/MainActivity.java
+++ app/src/main/java/rocks/talha/taptaptap/MainActivity.java
@@ -3,23 +3,25 @@ package rocks.talha.taptaptap;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import rocks.talha.taptaptap.view.TapView;
 
 public class MainActivity extends Activity {
     private TapView gameView;
+    public TextView highScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
         RelativeLayout layout = findViewById(R.id.relativeLayout);
         gameView = new TapView(this, getPreferences(Context.MODE_PRIVATE), layout);
-
         layout.addView(gameView, 0);
 
     }
